70cf0c22424fc312264fae1ec9eb08d776b2ba57
EC2 Default User <ec2-user@ip-172-31-11-11.ec2.internal>
2022-04-07 04:32:43 +0000

    ６章　　終わり

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,7 +8,8 @@ class ListsController < ApplicationController
 
     list.save
 
-    redirect_to '/top'
+    redirect_to list_path(list.id)
+
   end
 
   def index
@@ -16,13 +17,22 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list=List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
   end
 
   private
+
   def list_params
-    params.require(:list).permit(:title,:body)
+    params.require(:list).permit(:title,:body,:image)
   end
 end
